d3e79d08b4ab323a73b11cef4d676c00a6d20c17
niti <cniti8990@gmail.com>
2020-08-15 09:15:03 -0400

    hello

---
+++ mynewprg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mynewprg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World hhi hi hi hi!");
+        }
+    }
+}
